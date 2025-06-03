838d43559ff6094d2bc69a8fe1e62a7d643ad586
Your Name <you@example.com>
2025-06-03 07:37:10 -0700

    Implement saturdayFun, mondayWork, and wrapAdjective functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("go hiking")); // "This Saturday, I want to go hiking!"
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "special") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
